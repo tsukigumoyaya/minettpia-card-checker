53096c24422303b1db8299a6acbfd6f0c6a8de1c
tsukigumoyaya <tsukigumoyaya@gmail.com>
2026-06-23 13:08:50 +0900

    viteの設定修正

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/minettpiaCardChecker/',
+  base: '/minettpia-card-checker/',
   plugins: [vue(), tailwindcss()],
 })
